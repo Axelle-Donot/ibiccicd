32cdf9d6f24f638be32f6b3a60ebb2232d54b823
gaetan <gaetancorso@laposte.net>
2025-02-05 16:39:38 +0100

    refactor feature_2 back/front

--- back/server.js
+++ back/server.js
@@ -12,6 +12,20 @@ app.get('/', (req, res) => {
   res.send('Hello from Backend!');
 });
 
+app.get('/feature2/:brand', async (req,res) =>{
+    const { brand } = req.params;
+    const apiUrl = `https://vpic.nhtsa.dot.gov/api/vehicles/GetVehicleTypesForMake/${brand}?format=json`;
+
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      res.json(data); // Renvoie la réponse de l'API externe
+    } catch (error) {
+      res.status(500).json({ error: "Erreur lors de l'appel à l'API externe" });
+    }
+});
+
+
 app.listen(port, () => {
   console.log(`Backend is running on http://localhost:${port}`);
 });
